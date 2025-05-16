0bdbe757b271e4f160d0a4060b7ed27e40b6c3e6
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-15 17:44:39 -0700

    Revert "fix: 8 - DEREFERENCE_NULLABLE - return value.getClass(); - dereferenced expression value is @Nullable"
    This reverts commit 7427e0f0d066d71803fa625dacbb140e4674a1e8.

--- src/main/java/org/mockito/internal/stubbing/answers/Returns.java
+++ src/main/java/org/mockito/internal/stubbing/answers/Returns.java
@@ -48,16 +48,10 @@ public class Returns implements Answer<Object>, ValidableAnswer, Serializable {
   }
 
   private String printReturnType() {
-    if (value == null) {
-      return "null";
-    }
     return value.getClass().getSimpleName();
   }
 
   private Class<?> returnType() {
-    if (value == null) {
-      throw new NullPointerException("Value is null");
-    }
     return value.getClass();
   }
 
