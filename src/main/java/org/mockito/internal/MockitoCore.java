98430935ed3b114c5593be350c73cd0c141ae721
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-15 17:50:05 -0700

    Revert "fix: 26 - DEREFERENCE_NULLABLE - List<Invocation> allInvocations = ongoingStubbing.getRegisteredInvocations(); - dereferenced expression ongoingStubbing is @Nullable"
    This reverts commit cf97ad43139847b5f9fd4d4a602894ae6630f550.

--- src/main/java/org/mockito/internal/MockitoCore.java
+++ src/main/java/org/mockito/internal/MockitoCore.java
@@ -252,10 +252,7 @@ public class MockitoCore {
    */
   public Invocation getLastInvocation() {
     OngoingStubbingImpl ongoingStubbing =
-        (OngoingStubbingImpl) mockingProgress().pullOngoingStubbing();
-    if (ongoingStubbing == null) {
-      throw new NullPointerException("OngoingStubbingImpl is null");
-    }
+        ((OngoingStubbingImpl) mockingProgress().pullOngoingStubbing());
     List<Invocation> allInvocations = ongoingStubbing.getRegisteredInvocations();
     return allInvocations.get(allInvocations.size() - 1);
   }
