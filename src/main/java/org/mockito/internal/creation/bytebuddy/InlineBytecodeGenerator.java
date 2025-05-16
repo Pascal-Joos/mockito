409e89d477d40e45d2422cc32f41c6f1026a0665
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-15 17:45:59 -0700

    Revert "fix: 17 - DEREFERENCE_NULLABLE - getModule.invoke( - dereferenced expression getModule is @Nullable"
    This reverts commit e6dba718a70f49dc0842db960e41ff5c76f017b8.

--- src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
@@ -271,9 +271,6 @@ public class InlineBytecodeGenerator implements BytecodeGenerator, ClassFileTran
     }
     Set<Object> modules = new HashSet<Object>();
     try {
-      if (getModule == null || canRead == null) {
-        return;
-      }
       Object target =
           getModule.invoke(
               Class.forName(
