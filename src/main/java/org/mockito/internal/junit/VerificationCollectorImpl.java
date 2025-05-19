2791b3532bc4e324b180e70c773779448c5e9e1f
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:16:06 -0700

    fix: 31 - DEREFERENCE_NULLABLE - .append(message.trim()) - dereferenced expression message is @Nullable

--- src/main/java/org/mockito/internal/junit/VerificationCollectorImpl.java
+++ src/main/java/org/mockito/internal/junit/VerificationCollectorImpl.java
@@ -6,7 +6,6 @@ package org.mockito.internal.junit;
 
 import static org.mockito.internal.progress.ThreadSafeMockingProgress.mockingProgress;
 
-import javax.annotation.Nullable;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 import org.mockito.exceptions.base.MockitoAssertionError;
@@ -73,14 +72,16 @@ public class VerificationCollectorImpl implements VerificationCollector {
     this.numberOfFailures = 0;
   }
 
-  private void append(@Nullable String message) {
-    this.numberOfFailures++;
-    this.builder
-        .append('\n')
-        .append(this.numberOfFailures)
-        .append(". ")
-        .append(message.trim())
-        .append('\n');
+  private void append(String message) {
+    if (message != null) {
+      this.numberOfFailures++;
+      this.builder
+          .append('\n')
+          .append(this.numberOfFailures)
+          .append(". ")
+          .append(message.trim())
+          .append('\n');
+    }
   }
 
   private class VerificationWrapper implements VerificationMode {
