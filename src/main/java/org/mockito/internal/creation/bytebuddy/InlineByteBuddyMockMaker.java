05f586903c8937dfc0477c3eb3425b30976350a3
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:16:18 -0700

    Revert "fix: 34 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method org.mockito.plugins.MockMaker.<T>createMock(org.mockito.mock.MockCreationSettings<T>,org.mockito.invocation.MockHandler) returns @NonNull"
    This reverts commit d6058e3fd1c83e5177afffaf4a2bcae595a5b0b0.

--- src/main/java/org/mockito/internal/creation/bytebuddy/InlineByteBuddyMockMaker.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InlineByteBuddyMockMaker.java
@@ -296,13 +296,10 @@ public class InlineByteBuddyMockMaker
             true);
   }
 
+  @Nullable
   @Override
   public <T> T createMock(MockCreationSettings<T> settings, MockHandler handler) {
-    T mockInstance = doCreateMock(settings, handler, false);
-    if (mockInstance == null) {
-      throw new NullPointerException("Mock instance cannot be null");
-    }
-    return mockInstance;
+    return doCreateMock(settings, handler, false);
   }
 
   @Override
