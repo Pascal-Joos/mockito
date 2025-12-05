ccba98a64b5db330fac9f67a331422b0ef6e20f9
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 14:39:50 +0000

    fix: 32 - PASS_NULLABLE - assureArguments( - passing @Nullable parameter 'Modifier.isStatic(field.getModifiers()) ? null : target' where @NonNull is required

--- src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
+++ src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
@@ -220,7 +220,7 @@ class InstrumentationMemberAccessor implements MemberAccessor {
   public void set(Field field, Object target, Object value) throws IllegalAccessException {
     assureArguments(
         field,
-        Modifier.isStatic(field.getModifiers()) ? null : target,
+        Modifier.isStatic(field.getModifiers()) ? field.getDeclaringClass() : target,
         field.getDeclaringClass(),
         new Object[] {value},
         new Class<?>[] {field.getType()});
