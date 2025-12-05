c08f359e882c23c3d892fb12754ff6193e5f65e6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 13:42:21 +0000

    fix: 8 - DEREFERENCE_NULLABLE - getModule.invoke( - dereferenced expression getModule is @Nullable

--- src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
@@ -266,7 +266,7 @@ public class InlineBytecodeGenerator implements BytecodeGenerator, ClassFileTran
   }
 
   private void assureCanReadMockito(Set<Class<?>> types) {
-    if (redefineModule == null) {
+    if (redefineModule == null || getModule == null || canRead == null) {
       return;
     }
     Set<Object> modules = new HashSet<Object>();
