c6737419c6d93fc9c23ab96c438b96f66ce3a044
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 14:42:07 +0000

    Revert "fix: 33 - ASSIGN_FIELD_NULLABLE - this.wanted = wanted; - assigning @Nullable expression to @NonNull field"
    This reverts commit d8991a155418b5fbbf40895242f7c3e2fc09cbee.

--- src/main/java/org/mockito/internal/verification/VerificationDataImpl.java
+++ src/main/java/org/mockito/internal/verification/VerificationDataImpl.java
@@ -22,9 +22,6 @@ public class VerificationDataImpl implements VerificationData {
 
   public VerificationDataImpl(
       InvocationContainerImpl invocations, @Nullable InvocationMatcher wanted) {
-    if (wanted == null) {
-      throw new IllegalArgumentException("wanted must not be null");
-    }
     this.invocations = invocations;
     this.wanted = wanted;
     this.assertWantedIsVerifiable();
