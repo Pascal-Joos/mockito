850c2bb2bbd37a0b5ae56fab4cadd643be2109ef
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 13:44:14 +0000

    fix: 9 - PASS_NULLABLE - hideRecursiveCall( - passing @Nullable parameter 'cause' where @NonNull is required

--- src/main/java/org/mockito/internal/creation/bytebuddy/MockMethodAdvice.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/MockMethodAdvice.java
@@ -325,6 +325,9 @@ public class MockMethodAdvice extends MockMethodDispatcher {
       return accessor.invoke(origin, instance, arguments);
     } catch (InvocationTargetException exception) {
       Throwable cause = exception.getCause();
+      if (cause == null) {
+        cause = exception;
+      }
       new ConditionalStackTraceFilter()
           .filter(
               hideRecursiveCall(
