228c2a42273f601a4833775e857e6af140734bef
Pascal-Joos <pascal.joos@outlook.de>
2025-12-05 13:30:21 +0000

    fix: 2 - DEREFERENCE_NULLABLE - List<Invocation> allInvocations = ongoingStubbing.getRegisteredInvocations(); - dereferenced expression ongoingStubbing is @Nullable

--- src/main/java/org/mockito/internal/MockitoCore.java
+++ src/main/java/org/mockito/internal/MockitoCore.java
@@ -251,8 +251,11 @@ public class MockitoCore {
    * @return last invocation
    */
   public Invocation getLastInvocation() {
-    OngoingStubbingImpl ongoingStubbing =
-        ((OngoingStubbingImpl) mockingProgress().pullOngoingStubbing());
+    OngoingStubbing<?> stubbing = mockingProgress().pullOngoingStubbing();
+    if (!(stubbing instanceof OngoingStubbingImpl)) {
+      throw new IllegalStateException("No ongoing stubbing available");
+    }
+    OngoingStubbingImpl ongoingStubbing = (OngoingStubbingImpl) stubbing;
     List<Invocation> allInvocations = ongoingStubbing.getRegisteredInvocations();
     return allInvocations.get(allInvocations.size() - 1);
   }
