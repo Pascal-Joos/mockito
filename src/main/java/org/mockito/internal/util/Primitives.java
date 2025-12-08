4067fb2b454432b99b43766ef470dc2e37237e77
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 14:40:26 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/util/Primitives.java:33     return (Class<T>) PRIMITIVE_TYPES.get(clazz);

--- src/main/java/org/mockito/internal/util/Primitives.java
+++ src/main/java/org/mockito/internal/util/Primitives.java
@@ -26,6 +26,7 @@ public class Primitives {
    * @param <T> The type
    * @return The primitive type if relevant, otherwise <code>null</code>
    */
+  @Nullable
   public static <T> Class<T> primitiveTypeOf(Class<T> clazz) {
     if (clazz.isPrimitive()) {
       return clazz;
@@ -45,8 +46,12 @@ public class Primitives {
 
   public static boolean isAssignableFromWrapper(Class<?> valueClass, Class<?> referenceType) {
     if (isPrimitiveOrWrapper(valueClass) && isPrimitiveOrWrapper(referenceType)) {
-      return Primitives.primitiveTypeOf(valueClass)
-          .isAssignableFrom(Primitives.primitiveTypeOf(referenceType));
+      Class<?> valuePrimitive = primitiveTypeOf(valueClass);
+      Class<?> referencePrimitive = primitiveTypeOf(referenceType);
+      if (valuePrimitive == null || referencePrimitive == null) {
+        return false;
+      }
+      return valuePrimitive.isAssignableFrom(referencePrimitive);
     }
     return false;
   }
