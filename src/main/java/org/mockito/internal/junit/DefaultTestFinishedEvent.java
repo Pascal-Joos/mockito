a629d4be7c3fc5c122fddb2a1518148cd19d8da2
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 14:26:28 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression testClassInstance is @Nullable' /home/vscode/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java:29     return testClassInstance.getClass().getSimpleName() + "." + testMethodName;

--- src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java
+++ src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java
@@ -26,6 +26,10 @@ public class DefaultTestFinishedEvent implements TestFinishedEvent {
 
   @Override
   public String getTestName() {
-    return testClassInstance.getClass().getSimpleName() + "." + testMethodName;
+    String className =
+        (testClassInstance != null)
+            ? testClassInstance.getClass().getSimpleName()
+            : "UnknownTestClass";
+    return className + "." + testMethodName;
   }
 }
