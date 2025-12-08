300b78229b13a8d33b126ee5b3efdcd341bbf00a
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 14:46:07 +0000

    fix: Type='PASS_NULLABLE', message='passing @Nullable parameter 'Modifier.isStatic(field.getModifiers()) ? null : target' where @NonNull is required' /home/vscode/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java:196     assureArguments(

--- src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
+++ src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
@@ -281,7 +281,11 @@ class InstrumentationMemberAccessor implements MemberAccessor {
   }
 
   private static void assureArguments(
-      AccessibleObject target, Object owner, Class<?> type, Object[] values, Class<?>[] types) {
+      AccessibleObject target,
+      @Nullable Object owner,
+      Class<?> type,
+      Object[] values,
+      Class<?>[] types) {
     if (owner != null) {
       if (!type.isAssignableFrom(owner.getClass())) {
         throw new IllegalArgumentException("Cannot access " + target + " on " + owner);
