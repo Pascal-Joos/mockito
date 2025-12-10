202343c65934f52c1d0368674840aae7ecc19fea
Pascal-Joos <pascal.joos@outlook.de>
2025-12-10 14:57:42 +0000

    Revert "fix: 14 - DEREFERENCE_NULLABLE - + MockUtil.getMockName(undesired.getMock()) - dereferenced expression undesired is @Nullable"
    This reverts commit 5145d98bc785c94f8a7d4549606c55348eead00d.

--- src/main/java/org/mockito/internal/exceptions/Reporter.java
+++ src/main/java/org/mockito/internal/exceptions/Reporter.java
@@ -8,7 +8,6 @@ import static org.mockito.internal.reporting.Pluralizer.pluralize;
 import static org.mockito.internal.reporting.Pluralizer.were_exactly_x_interactions;
 import static org.mockito.internal.util.StringUtil.join;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -473,7 +472,7 @@ public class Reporter {
             "No interactions wanted here:",
             new LocationImpl(),
             "But found this interaction on mock '"
-                + MockUtil.getMockName(Nullability.castToNonnull(undesired).getMock())
+                + MockUtil.getMockName(undesired.getMock())
                 + "':",
             undesired.getLocation(),
             scenario));
