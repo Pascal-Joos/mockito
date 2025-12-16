f4c3ab860444adc907e10126b62630399528155b
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:24:15 +0000

    fix: 30 - PASS_NULLABLE - assureArguments(constructor, null, null, arguments, constructor.getParameterTypes()); - passing @Nullable parameter 'null' where @NonNull is required

--- src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
+++ src/main/java/org/mockito/internal/util/reflection/InstrumentationMemberAccessor.java
@@ -128,7 +128,8 @@ class InstrumentationMemberAccessor implements MemberAccessor {
       throw new InstantiationException(
           "Cannot instantiate abstract " + constructor.getDeclaringClass().getTypeName());
     }
-    assureArguments(constructor, null, null, arguments, constructor.getParameterTypes());
+    assureArguments(
+        constructor, new Object[0], new Class<?>[0], arguments, constructor.getParameterTypes());
     try {
       Object module = getModule.bindTo(constructor.getDeclaringClass()).invokeWithArguments();
       String packageName = constructor.getDeclaringClass().getPackage().getName();
