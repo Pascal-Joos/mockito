775bea50f01953049b0bd103967d90768d04d7d1
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:14:54 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression canRead is @Nullable' /home/vscode/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java:282         if (!modules.contains(module) && !(Boolean) canRead.invoke(module, target)) {

--- src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InlineBytecodeGenerator.java
@@ -266,7 +266,7 @@ public class InlineBytecodeGenerator implements BytecodeGenerator, ClassFileTran
   }
 
   private void assureCanReadMockito(Set<Class<?>> types) {
-    if (redefineModule == null) {
+    if (redefineModule == null || canRead == null) {
       return;
     }
     Set<Object> modules = new HashSet<Object>();
@@ -279,7 +279,8 @@ public class InlineBytecodeGenerator implements BytecodeGenerator, ClassFileTran
                   null));
       for (Class<?> type : types) {
         Object module = getModule.invoke(type);
-        if (!modules.contains(module) && !(Boolean) canRead.invoke(module, target)) {
+        if (!modules.contains(module)
+            && Boolean.TRUE.equals((Boolean) canRead.invoke(module, target))) {
           modules.add(module);
         }
       }
