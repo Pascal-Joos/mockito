4ac8da5fca04662f3d6a8a8ab169117eb45a3fd8
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:20:19 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression value is @Nullable' /home/vscode/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/stubbing/answers/Returns.java:51     return value.getClass().getSimpleName();

--- src/main/java/org/mockito/internal/stubbing/answers/Returns.java
+++ src/main/java/org/mockito/internal/stubbing/answers/Returns.java
@@ -48,7 +48,7 @@ public class Returns implements Answer<Object>, ValidableAnswer, Serializable {
   }
 
   private String printReturnType() {
-    return value.getClass().getSimpleName();
+    return value == null ? "null" : value.getClass().getSimpleName();
   }
 
   private Class<?> returnType() {
