86e78a1a184b1e65328394051708c03a4435345e
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 11:05:55 +0000

    fix: 21 - ASSIGN_FIELD_NULLABLE - private Object delegatedObject = null; - assigning @Nullable expression to @NonNull field

--- src/main/java/org/mockito/internal/stubbing/defaultanswers/ForwardsInvocations.java
+++ src/main/java/org/mockito/internal/stubbing/defaultanswers/ForwardsInvocations.java
@@ -10,6 +10,7 @@ import static org.mockito.internal.exceptions.Reporter.delegatedMethodHasWrongRe
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import javax.annotation.Nullable;
 import org.mockito.internal.configuration.plugins.Plugins;
 import org.mockito.invocation.Invocation;
 import org.mockito.invocation.InvocationOnMock;
@@ -24,7 +25,7 @@ import org.mockito.stubbing.Answer;
 public class ForwardsInvocations implements Answer<Object>, Serializable {
   private static final long serialVersionUID = -8343690268123254910L;
 
-  private Object delegatedObject = null;
+  @Nullable private Object delegatedObject = null;
 
   public ForwardsInvocations(Object delegatedObject) {
     this.delegatedObject = delegatedObject;
@@ -33,20 +34,25 @@ public class ForwardsInvocations implements Answer<Object>, Serializable {
   public Object answer(InvocationOnMock invocation) throws Throwable {
     Method mockMethod = invocation.getMethod();
 
+    if (delegatedObject == null) {
+      throw delegatedMethodDoesNotExistOnDelegate(mockMethod, invocation.getMock(), null);
+    }
+
+    Object nonNullDelegatedObject = delegatedObject;
     try {
       Method delegateMethod = getDelegateMethod(mockMethod);
 
       if (!compatibleReturnTypes(mockMethod.getReturnType(), delegateMethod.getReturnType())) {
         throw delegatedMethodHasWrongReturnType(
-            mockMethod, delegateMethod, invocation.getMock(), delegatedObject);
+            mockMethod, delegateMethod, invocation.getMock(), nonNullDelegatedObject);
       }
 
       MemberAccessor accessor = Plugins.getMemberAccessor();
       Object[] rawArguments = ((Invocation) invocation).getRawArguments();
-      return accessor.invoke(delegateMethod, delegatedObject, rawArguments);
+      return accessor.invoke(delegateMethod, nonNullDelegatedObject, rawArguments);
     } catch (NoSuchMethodException e) {
       throw delegatedMethodDoesNotExistOnDelegate(
-          mockMethod, invocation.getMock(), delegatedObject);
+          mockMethod, invocation.getMock(), nonNullDelegatedObject);
     } catch (InvocationTargetException e) {
       // propagate the original exception from the delegate
       throw e.getCause();
@@ -54,12 +60,14 @@ public class ForwardsInvocations implements Answer<Object>, Serializable {
   }
 
   private Method getDelegateMethod(Method mockMethod) throws NoSuchMethodException {
-    if (mockMethod.getDeclaringClass().isAssignableFrom(delegatedObject.getClass())) {
+    Object localDelegatedObject = delegatedObject;
+    if (localDelegatedObject != null
+        && mockMethod.getDeclaringClass().isAssignableFrom(localDelegatedObject.getClass())) {
       // Compatible class. Return original method.
       return mockMethod;
     } else {
       // Return method of delegate object with the same signature as mockMethod.
-      return delegatedObject
+      return localDelegatedObject
           .getClass()
           .getMethod(mockMethod.getName(), mockMethod.getParameterTypes());
     }
