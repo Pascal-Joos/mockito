cf87be12722bc3454f31bc2f1b6ad2e1514650b0
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 10:58:53 +0000

    Revert "fix: 15 - DEREFERENCE_NULLABLE - return testClassInstance.getClass().getSimpleName() + "." + testMethodName; - dereferenced expression testClassInstance is @Nullable"
    This reverts commit b6417d38a93c3f9c7a39799b4e77a412a9d2d4d6.

--- src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java
+++ src/main/java/org/mockito/internal/junit/DefaultTestFinishedEvent.java
@@ -26,8 +26,6 @@ public class DefaultTestFinishedEvent implements TestFinishedEvent {
 
   @Override
   public String getTestName() {
-    return (testClassInstance == null ? "null" : testClassInstance.getClass().getSimpleName())
-        + "."
-        + testMethodName;
+    return testClassInstance.getClass().getSimpleName() + "." + testMethodName;
   }
 }
