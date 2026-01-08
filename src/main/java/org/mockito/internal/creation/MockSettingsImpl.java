dd03e3b2403cfd30c7c1fb8e47476f7d8428f7ed
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:59:20 +0000

    fix: 3 - DEREFERENCE_NULLABLE - List<Object> resultArgs = new ArrayList<Object>(constructorArgs.length + 1); - dereferenced expression constructorArgs is @Nullable

--- src/main/java/org/mockito/internal/creation/MockSettingsImpl.java
+++ src/main/java/org/mockito/internal/creation/MockSettingsImpl.java
@@ -159,6 +159,9 @@ public class MockSettingsImpl<T> extends CreationSettings<T>
     if (outerClassInstance == null) {
       return constructorArgs;
     }
+    if (constructorArgs == null) {
+      return null;
+    }
     List<Object> resultArgs = new ArrayList<Object>(constructorArgs.length + 1);
     resultArgs.add(outerClassInstance);
     resultArgs.addAll(asList(constructorArgs));
