824815c32ad40e231f3f97e17e31fcab8e8e67ee
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:55:30 +0000

    Revert "fix: 1 - ASSIGN_FIELD_NULLABLE - this.mockitoOngoingStubbing = ongoingStubbing; - assigning @Nullable expression to @NonNull field"
    This reverts commit a2f605b8b2ea3e6e545bce6dfb3ec7ad45334c01.

--- src/main/java/org/mockito/BDDMockito.java
+++ src/main/java/org/mockito/BDDMockito.java
@@ -4,7 +4,6 @@
  */
 package org.mockito;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 import org.mockito.stubbing.Answer;
 import org.mockito.stubbing.OngoingStubbing;
@@ -169,76 +168,48 @@ public class BDDMockito extends Mockito {
 
   private static class BDDOngoingStubbingImpl<T> implements BDDMyOngoingStubbing<T> {
 
-    @Nullable private final OngoingStubbing<T> mockitoOngoingStubbing;
+    private final OngoingStubbing<T> mockitoOngoingStubbing;
 
     public BDDOngoingStubbingImpl(@Nullable OngoingStubbing<T> ongoingStubbing) {
       this.mockitoOngoingStubbing = ongoingStubbing;
     }
 
     public BDDMyOngoingStubbing<T> willAnswer(Answer<?> answer) {
-      if (mockitoOngoingStubbing == null) {
-        return null;
-      }
-      return new BDDOngoingStubbingImpl<T>(
-          Nullability.castToNonnull(mockitoOngoingStubbing).thenAnswer(answer));
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.thenAnswer(answer));
     }
 
     public BDDMyOngoingStubbing<T> will(Answer<?> answer) {
-      if (mockitoOngoingStubbing == null) {
-        return new BDDOngoingStubbingImpl<T>(null);
-      }
-      return new BDDOngoingStubbingImpl<T>(
-          Nullability.castToNonnull(mockitoOngoingStubbing).then(answer));
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.then(answer));
     }
 
     public BDDMyOngoingStubbing<T> willReturn(T value) {
-      if (mockitoOngoingStubbing == null) {
-        throw new NullPointerException("mockitoOngoingStubbing");
-      }
-      return new BDDOngoingStubbingImpl<T>(
-          Nullability.castToNonnull(mockitoOngoingStubbing).thenReturn(value));
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.thenReturn(value));
     }
 
     public BDDMyOngoingStubbing<T> willReturn(T value, T... values) {
-      if (mockitoOngoingStubbing == null) {
-        return null;
-      }
-      return new BDDOngoingStubbingImpl<T>(
-          Nullability.castToNonnull(mockitoOngoingStubbing).thenReturn(value, values));
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.thenReturn(value, values));
     }
 
     public BDDMyOngoingStubbing<T> willThrow(Throwable... throwables) {
-      if (mockitoOngoingStubbing == null) {
-        return null;
-      }
-      return new BDDOngoingStubbingImpl<T>(
-          Nullability.castToNonnull(mockitoOngoingStubbing).thenThrow(throwables));
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.thenThrow(throwables));
     }
 
     public BDDMyOngoingStubbing<T> willThrow(Class<? extends Throwable> throwableType) {
-      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing).willThrow(throwableType);
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.thenThrow(throwableType));
     }
 
     public BDDMyOngoingStubbing<T> willThrow(
         Class<? extends Throwable> throwableType, Class<? extends Throwable>... throwableTypes) {
-      if (mockitoOngoingStubbing == null) {
-        return null;
-      }
       return new BDDOngoingStubbingImpl<T>(
           mockitoOngoingStubbing.thenThrow(throwableType, throwableTypes));
     }
 
     public BDDMyOngoingStubbing<T> willCallRealMethod() {
-      OngoingStubbing<T> ongoingStubbing = mockitoOngoingStubbing;
-      if (ongoingStubbing == null) {
-        throw new NullPointerException("mockitoOngoingStubbing");
-      }
-      return new BDDOngoingStubbingImpl<T>(
-          Nullability.castToNonnull(mockitoOngoingStubbing).thenCallRealMethod());
+      return new BDDOngoingStubbingImpl<T>(mockitoOngoingStubbing.thenCallRealMethod());
     }
 
     public <M> M getMock() {
-      return (M) Nullability.castToNonnull(mockitoOngoingStubbing).getMock();
+      return (M) mockitoOngoingStubbing.getMock();
     }
   }
 
