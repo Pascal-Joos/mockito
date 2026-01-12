b6a3287f75462db54ba275536a2475ddc243a32b
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 08:18:43 +0000

    fix: Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/vscode/nullness-benchmarks/mockito/src/main/java/org/mockito/internal/util/reflection/FieldInitializationReport.java:54     return fieldInstance != null ? fieldInstance.getClass() : null;

--- src/main/java/org/mockito/internal/configuration/injection/PropertyAndSetterInjection.java
+++ src/main/java/org/mockito/internal/configuration/injection/PropertyAndSetterInjection.java
@@ -9,6 +9,7 @@ import static org.mockito.internal.exceptions.Reporter.fieldInitialisationThrewE
 import static org.mockito.internal.util.collections.Sets.newMockSafeHashSet;
 import static org.mockito.internal.util.reflection.SuperTypesLastSorter.sortSuperTypesLast;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
@@ -77,6 +78,9 @@ public class PropertyAndSetterInjection extends MockInjectionStrategy {
     // for each field in the class hierarchy
     boolean injectionOccurred = false;
     Class<?> fieldClass = report.fieldClass();
+    if (fieldClass == null) {
+      return false;
+    }
     Object fieldInstanceNeedingInjection = report.fieldInstance();
     while (fieldClass != Object.class) {
       injectionOccurred |=
@@ -102,7 +106,8 @@ public class PropertyAndSetterInjection extends MockInjectionStrategy {
   private boolean injectMockCandidates(
       Class<?> awaitingInjectionClazz, Object injectee, Set<Object> mocks) {
     boolean injectionOccurred;
-    List<Field> orderedCandidateInjecteeFields = orderedInstanceFieldsFrom(awaitingInjectionClazz);
+    List<Field> orderedCandidateInjecteeFields =
+        orderedInstanceFieldsFrom(Nullability.castToNonnull(awaitingInjectionClazz));
     // pass 1
     injectionOccurred =
         injectMockCandidatesOnFields(mocks, injectee, false, orderedCandidateInjecteeFields);
